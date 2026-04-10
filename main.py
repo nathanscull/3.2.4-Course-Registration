9d7b01dd96eca5860e4d51b3465e2b1ae90c1cb2
nathanscull <nathanscull11@gmail.com>
2026-04-10 15:42:54 +0000

    Refactor main program structure for course registration assignments

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 done = False
+while done == False:
+    def assignment1():
+        print("Assignment 1 Completed")
+    def assignment2():
+        return("Assignment 2 Completed")
 
-def assignment1():
-    print("Assignment 1 Completed")
+    assignment_number = input("Enter 1 or 2 or quit: ")
+    
+    def execute_assignment(assignment_number):
+        if assignment_number == "1":
+            assignment1()
+        elif assignment_number == "2":
+            print(assignment2())
+        elif assignment_number == "quit":
+            global done
+            done = True
+        else:
+            print("Invalid input. Please enter 1, 2, or quit.")
 
-def assignment2():
-    print("Assignment 2 Completed")
-
-while not done:
-    assignment_number = input("1, 2 or quit? ")
-
-    if assignment_number == "1":
-        assignment1()
-    elif assignment_number == "2":
-        assignment2()
-    elif assignment_number == "quit":
-        done = True
-    else:
-        print("Invalid input. Please enter 1, 2 or quit.")
+    execute_assignment(assignment_number)
